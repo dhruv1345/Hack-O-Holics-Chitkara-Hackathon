9fb16906fbf01aa4fd8eb087549578c815d0d601
Dhruv Gupta <86596711+dhruv1345@users.noreply.github.com>
2022-12-24 13:58:28 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:loginuiapp/main_page.dart';
+import 'LoginPage.dart';
+
+void main() {
+  runApp(
+    LoginApp(),
+  );
+}
+
+class LoginApp extends StatelessWidget {
+  const LoginApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
